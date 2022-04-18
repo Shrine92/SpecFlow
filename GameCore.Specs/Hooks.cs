a78237e3e5962d4bd26697a0009ac4c4b21b71d9
Benjamin PRIELS <prlbnj@forem.be>
2022-04-18 11:09:45 +0200

    hooks

---
+++ GameCore.Specs/Hooks.cs
@@ -0,0 +1,26 @@
+﻿using TechTalk.SpecFlow;
+
+namespace GameCore.Specs
+{
+    [Binding]
+    public class Hooks : Steps
+    {
+        [BeforeScenario(Order = 100)]
+        public void BeforeScenario1()
+        {
+
+        }
+
+        [BeforeScenario(Order = 200)]
+        public void BeforeScenario2()
+        {
+
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+
+        }
+    }
+}
